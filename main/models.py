a0b7a9cbaa1dc708d835118af44b8cf52dc6fc9a
Zaharov Andrey <zaharov.andrey161@gmail.com>
2021-01-23 02:33:20 +0300

    Completed DataBase recording.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class StudentStats(models.Model):
+    hp = models.IntegerField(default=0)
+    iq = models.IntegerField(default=0)
+    ha = models.IntegerField(default=0)
+    date = models.DateTimeField(default=None)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from main.models import StudentStats
+from datetime import datetime
 
 # Create your views here.
+
 def clicker(request):
-    return render(request, 'pages/student.html')
+    lastStat = StudentStats.objects.last()
+    context = {
+        'hp': lastStat.hp,
+        'iq': lastStat.iq,
+        'ha': lastStat.ha
+        }
+    if request.method == 'POST':
+        hp = request.POST['hp_hidden']
+        iq = request.POST['iq_hidden']
+        ha = request.POST['ha_hidden']
+        record = StudentStats(hp=hp, iq=iq, ha=ha, date=datetime.now())
+        record.save()
+    return render(request, 'pages/student.html', context)
